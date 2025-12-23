a96c9062bb1d5287b6af72e6954fb8b0ab328fc6
yoshikazu1225 <kazuma20101225@gmail.com>
2025-12-23 20:58:56 +0900

    Add files via upload

---
+++ 機械学習.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#データ生成
+np.random.seed(seed=1) # 乱数を固定
+x = np.arange(10) # x を作成
+y = np.random.rand(10) # y を作成
+
+#グラフ描写
+plt.plot(x, y) # 折れ線グラフを登録
+plt.show() # グラフ表示
