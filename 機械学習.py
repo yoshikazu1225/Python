f0b633ee26f9281897da5c0a78e794704b1de5f3
yoshikazu1225 <kazuma20101225@gmail.com>
2025-12-24 01:08:55 +0900

    グラフを表示できるようにした

--- 機械学習.py
+++ 機械学習.py
@@ -1,11 +1,26 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-#データ生成
-np.random.seed(seed=1) # 乱数を固定
-x = np.arange(10) # x を作成
-y = np.random.rand(10) # y を作成
+# 関数を定義
+def f(x):
+    return (x - 2) * x * (x + 2)     # 関数ｆの定義
 
-#グラフ描写
-plt.plot(x, y) # 折れ線グラフを登録
-plt.show() # グラフ表示
+def f2(x):
+    return (x -1) * x * (x + 2)      # 関数ｆ2の定義
+
+# x の範囲を定義
+x = np.linspace(-3, 3, 100)          # xを100個準備
+
+# グラフを描写
+plt.plot(x, f(x), "black", label="$f$")    # fのグラフ
+plt.plot(x, f2(x), "cornflowerblue", label="$f_2$")  #f2のグラフ
+plt.legend(loc="upper left")         # 凡例表示
+plt.title("comparison of $f$ and $f_2$")  # タイトル
+plt.xlabel("x")                    # x軸ラベル
+plt.ylabel("y")                    # y軸ラベル
+plt.xlim(-3, 3)                    # x軸の範囲
+plt.ylim(-15, 15)                  # y軸の範囲
+plt.grid()                         # グリッド
+plt.show()
+
+
